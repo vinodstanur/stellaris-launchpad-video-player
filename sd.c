e4af596bf6a2c647581c6b839893af64e7c6e1cb
vinodstanur <vinodstanur@gmail.com>
2013-04-23 02:23:58 +0530

    modified

--- sd.c
+++ sd.c
@@ -1,9 +1,32 @@
+
+
 /*
  * sd.c
  *
  *  Created on: 04-Apr-2013
  *      Author: vinod
- */
+
+My sd card library....
+author: Vinod S
+email: vinodstanur at gmail dot com
+date: 23/4/2013
+homepage: http://blog.vinu.co.in
+compiler: arm-none-eabi-gcc (summon arm toolchain)
+Copyright (C) <2013> <http://blog.vinu.co.in>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include "SD.h"
 #include "TFT.h"
 
